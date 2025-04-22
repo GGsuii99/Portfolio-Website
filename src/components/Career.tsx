51e948d708f728bb4bc9cadcfa5bae85ff7fc621
GGsuii99 <123018@combertonvc.org>
2025-04-22 18:02:29 +0100

    Update Career.tsx

--- src/components/Career.tsx
+++ src/components/Career.tsx
@@ -24,6 +24,18 @@ const Career = () => {
               A very good supermarket. Sells nothing because I need to buy <a href="https://e.uk"></a> to get started
             </p>
           </div>
+          <div className="career-info-box">
+            <div className="career-info-in">
+              <div className="career-role">
+                <h4>The gamer at the back of the shop on the spare PC</h4>
+                <h5>Karadeniz Kitap Kirtasiye</h5>
+              </div>
+              <h3>2023</h3>
+            </div>
+            <p>
+              A very good stationery shop. 
+            </p>
+          </div>
           
         </div>
       </div>
